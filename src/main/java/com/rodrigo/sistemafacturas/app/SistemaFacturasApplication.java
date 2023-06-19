154925079819e504e7c4d93f7e86ed342e5aee41
Rodrigo Rodriguez <rod.rodriguef@gmail.com>
2023-06-19 14:55:03 -0400

    Initial commit

---
+++ src/main/java/com/rodrigo/sistemafacturas/app/SistemaFacturasApplication.java
@@ -0,0 +1,13 @@
+package com.rodrigo.sistemafacturas.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaFacturasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaFacturasApplication.class, args);
+	}
+
+}
