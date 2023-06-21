9bfcc2951433e0a68489f27b7952d7c518494197
Rodrigo Rodriguez <rod.rodriguef@gmail.com>
2023-06-20 23:26:58 -0400

    id_cliente_2

--- src/main/java/com/rodrigo/sistemafacturas/app/models/entity/Cliente.java
+++ src/main/java/com/rodrigo/sistemafacturas/app/models/entity/Cliente.java
@@ -7,7 +7,7 @@ import jakarta.persistence.*;
 public class Cliente {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long id_cliente_2;
 
     private String nombre;
     private String apellido;
